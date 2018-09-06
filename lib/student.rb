0f1f4621082daeefaa02c3c7aaa755cdf2413c70
Wendy Calderon <wendycalderon@gmail.com>
2018-09-06 00:57:27 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -29,6 +29,6 @@ class Student
       VALUES (?, ?)
     SQL
     DB[:conn].execute(sql, self.name, self.grade)
- 
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
   end 
 end
