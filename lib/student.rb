f3bfefe20c081d2035cc7bfd39b1a16606a368ff
Wendy Calderon <wendycalderon@gmail.com>
2018-09-06 00:56:13 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -24,5 +24,11 @@ class Student
   end 
   
   def save 
+    sql = <<-SQL
+      INSERT INTO students (name, grade) 
+      VALUES (?, ?)
+    SQL
+    DB[:conn].execute(sql, self.name, self.grade)
+ 
   end 
 end
