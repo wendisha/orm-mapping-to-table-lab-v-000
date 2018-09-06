3fca1986e0f5ef1ca476b6065034ea94b291ed43
Wendy Calderon <wendycalderon@gmail.com>
2018-09-06 01:21:22 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -39,8 +39,8 @@ class Student
   end 
   
   def self.create(name:, grade:)
-    student = Student.new(name, album)
-    song.save
-    song
+    student = Student.new(name, grade)
+    student.save
+    student
   end
 end
